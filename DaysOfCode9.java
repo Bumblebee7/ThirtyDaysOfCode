46dd157c7f7877364abff655a2d9f237c31ba631
Alex Mueller <muellea@hochschule-trier.de>
2017-08-29 13:23:50 +0200

    Added challenge 9

---
+++ DaysOfCode9.java
@@ -0,0 +1,32 @@
+package thirtyDaysOfCode;
+
+import java.util.Scanner;
+
+/*
+ * Task:
+ * Write a factorial function that takes a positive integer, N as a parameter and prints the result of N!(N factorial).
+ * 
+ * Input Format:
+ * A single integer, N (the argument to pass to factorial).
+*/
+
+public class DaysOfCode9 {
+
+	public static int factorial(int n) {
+		if (n > 1) // call next step of recursion
+			return (n * factorial(n - 1));
+		else // to end the recursion
+			return n;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		scan.close();
+
+		int result = factorial(num);
+		System.out.println(result);
+	}
+
+}
