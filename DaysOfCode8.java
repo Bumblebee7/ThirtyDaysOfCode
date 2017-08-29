4a3eb23294672ad269dfee5937b5cf399f56eed6
Alex Mueller <muellea@hochschule-trier.de>
2017-08-29 12:24:00 +0200

    Added comments in challenge 8

--- DaysOfCode8.java
+++ DaysOfCode8.java
@@ -4,6 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+ * Task:
+ * Given N names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. You will then be 
+ * given an unknown number of names to query your phone book for. For each name queried, print the associated entry from your phone book 
+ * on a new line in the form name=phoneNumber; if an entry for name is not found, print Not found instead.
+ * 
+ * Note: Your phone book should be a Dictionary/Map/HashMap data structure.
+ * 
+ * Input Format:
+ * The first line contains an integer, N , denoting the number of entries in the phone book.
+ * Each of the N subsequent lines describes an entry in the form of 2 space-separated values on a single line. The first value is a 
+ * friend's name, and the second value is an 8-digit phone number.
+ * 
+ * After the N lines of phone book entries, there are an unknown number of lines of queries. Each line (query) contains a name to look up, 
+ * and you must continue reading lines until there is no more input.
+ * 
+ * Note: Names consist of lowercase English alphabetic letters and are first names only.
+ */
+
 public class DaysOfCode8 {
 
 	public static void main(String[] argh) {
@@ -12,12 +31,16 @@ public class DaysOfCode8 {
 		int n = in.nextInt();
 		Map<String, Integer> phoneBook = new HashMap<String, Integer>();
 
+		// Input
 		for (int i = 0; i < n; i++) {
 			String name = in.next();
-			Integer phone = in.nextInt();
+			Integer phone = in.nextInt(); // Better use Integer (Object) because
+											// map works with Object not
+											// primitive data types like int
 			phoneBook.put(name, phone);
 		}
 
+		// Output
 		while (in.hasNext()) {
 			String s = in.next();
 			if (phoneBook.get(s) == null)
