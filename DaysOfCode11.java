2e7561379a62f2686d1608ba3c3e8c33a480ae42
Alex Mueller <muellea@hochschule-trier.de>
2017-08-31 11:02:05 +0200

    Added some comments.

--- DaysOfCode11.java
+++ DaysOfCode11.java
@@ -2,13 +2,36 @@ package thirtyDaysOfCode;
 
 import java.util.Scanner;
 
+/*
+ * Context:
+ * Given a 6 x 6 2D Array, A: 
+ * We define an hourglass in A to be a subset of values with indices falling in this pattern 
+ * in A's graphical representation:
+ * 
+ * a b c
+ *   d
+ * e f g
+ * 
+ * There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values. 
+ * 
+ * Task:
+ * Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+ * 
+ * Input Format:
+ * There are lines of input, where each line contains 6 space-separated integers describing 2D Array A;
+ * every value in will be in the inclusive range of -9 to 9.
+ * 
+ */
+
 public class DaysOfCode11 {
 
 	public static void main(String[] args) {
+
 		final int COLSIZE = 6, ROWSIZE = 6;
 		Scanner scan = new Scanner(System.in);
 		int arr[][] = new int[ROWSIZE][COLSIZE];
 
+		// Input
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 6; j++) {
 				arr[i][j] = scan.nextInt();
@@ -16,6 +39,7 @@ public class DaysOfCode11 {
 		}
 		scan.close();
 
+		// The minimum sum of an hourglass is 7 x -9 = -63
 		int biggest = -1000;
 
 		for (int j = 0; j < ROWSIZE - 2; j++) {
