0f339df47b5853ac83171fbf38c4cb6531572c11
Alex <alexboxxer@web.de>
2017-09-13 11:05:20 +0200

    Added Ex 25

---
+++ src/daysOfCode25/Solution.java
@@ -0,0 +1,40 @@
+package daysOfCode25;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		int[] values = new int[num];
+		for (int i = 0; i < num; i++) {
+			values[i] = scan.nextInt();
+		}
+		scan.close();
+
+		isPrime(values);
+	}
+
+	public static void isPrime(int[] values) {
+
+		for (int a : values) {
+			if (a <= 1)
+				System.out.println("Not prime");
+			else {
+				boolean isPrime = true;
+				for (int i = 3; i < a; i++) {
+					if (a % i == 0) {
+						isPrime = false;
+						break;
+					}
+				}
+				if (isPrime)
+					System.out.println("Prime");
+				else
+					System.out.println("Not prime");
+			}
+		}
+	}
+}
