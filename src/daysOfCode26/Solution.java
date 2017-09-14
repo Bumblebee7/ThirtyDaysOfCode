41e959bfd55df8cdbcebab66b5552e0de76dee86
Alex <alexboxxer@web.de>
2017-09-14 11:07:05 +0200

    Added ex 26

---
+++ src/daysOfCode26/Solution.java
@@ -0,0 +1,32 @@
+package daysOfCode26;
+
+import java.util.Scanner;
+
+// quick and dirty
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int ActualDay = scan.nextInt();
+		int ActualMonth = scan.nextInt();
+		int ActualYear = scan.nextInt();
+
+		int ExpectedDay = scan.nextInt();
+		int ExpectedMonth = scan.nextInt();
+		int ExpectedYear = scan.nextInt();
+
+		if (ActualYear > ExpectedYear)
+			System.out.println("10000");
+		else if (ActualYear == ExpectedYear) {
+			int fine = 0;
+			if (ActualMonth > ExpectedMonth)
+				fine += (Math.abs(ExpectedMonth - ActualMonth) * 500);
+			else if (ActualDay > ExpectedDay)
+				fine += (Math.abs(ExpectedDay - ActualDay) * 15);
+			System.out.println(fine);
+		} else
+			System.out.println("0");
+	}
+}
